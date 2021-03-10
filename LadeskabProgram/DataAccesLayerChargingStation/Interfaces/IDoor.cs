57710c4fd3b2c79f64fb3c47596633ec37669d24
LarsMoelgaard <60734425+LarsMoelgaard@users.noreply.github.com>
2021-03-10 09:40:32 +0100

    Interfaces

---
+++ LadeskabProgram/DataAccesLayerChargingStation/Interfaces/IDoor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccesLayerChargingStation.Interfaces
+{
+    public interface IDoor
+    {
+        public void LockDoor();
+
+        public void UnLockDoor();
+
+        //public void DoorOpened();
+
+        //public void DoorClosed();
+    }
+}
