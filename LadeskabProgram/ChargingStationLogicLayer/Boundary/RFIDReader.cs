38db17ca4210a5aad458e3228df239ad65b769ab
Signe Gram Sand <signegramsand@gmail.com>
2021-03-11 14:01:54 +0100

    Ændring i klasserne

---
+++ LadeskabProgram/ChargingStationLogicLayer/Boundary/RFIDReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PresentationsLag
+{
+    public class RFIDReader : IRFID
+    {
+        public void OnRfidRead(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LadeskabProgram/ChargingStationLogicLayer/Controllers/ChargeControl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer.Controllers
+{
+    public class ChargeControl
+    {
+    }
+}
