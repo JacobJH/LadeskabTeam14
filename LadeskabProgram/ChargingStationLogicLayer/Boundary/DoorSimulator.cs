ab112f331c7b0ddeb091298511a01f600e758e55
lassees <lasse.g.eskildsen@gmail.com>
2021-03-11 13:36:24 +0100

    Changed folder structure

---
+++ LadeskabProgram/ChargingStationLogicLayer/Boundary/DoorSimulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataAccesLayerChargingStation.Interfaces
+{
+    public class DoorSimulator : IDoor
+    {
+        public DoorSimulator()
+        {
+        }
+
+        public void UnlockDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDoorOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDoorClose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LockDoor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LadeskabProgram/ChargingStationLogicLayer/Boundary/Interfaces/IDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccesLayerChargingStation.Interfaces
+{
+    public interface IDoor
+    {
+        public void LockDoor();
+        
+        public void UnlockDoor();
+
+        public void OnDoorOpen();
+
+        public void OnDoorClose();
+    }
+}
