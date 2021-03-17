525d997404ba3d2b327f1e42dc2a4cd590f4ead8
lassees <lasse.g.eskildsen@gmail.com>
2021-03-17 08:32:45 +0100

    Added filelogger

---
+++ LadeskabProgram/ChargingStationLogicLayer/Boundary/FileLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LogicLayer.Boundary.Interfaces
+{
+    public class FileLogger : ILogger
+    {
+        public void LogDoorLocked(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogDoorUnLocked(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LadeskabProgram/ChargingStationLogicLayer/Boundary/Interfaces/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer.Boundary.Interfaces
+{
+    public interface ILogger
+    {
+        public void LogDoorLocked(int id);
+        public void LogDoorUnLocked(int id);
+
+    }
+}
