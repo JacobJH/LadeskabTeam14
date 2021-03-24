d7d4fedea46190a3852f197c237e669b1df5fe7e
JacobJH <60734597+JacobJH@users.noreply.github.com>
2021-03-24 09:37:11 +0100

    ChargeControl with event and test

---
+++ LadeskabProgram/EventArgs/USBConnectedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventArguments
+{
+    public class USBConnectedEventArgs
+    {
+        public bool Connected { get; set; }
+    }
+}
